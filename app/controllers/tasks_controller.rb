423f89ef076d14d48589cd00467b344ad2676626
DaichiSaito <hrkedz@gmail.com>
2022-04-28 11:37:12 +0900

    削除機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,12 +11,19 @@ class TasksController < ApplicationController
       # 第一引数はturbo_stream_fromの引数と同じにする
       # targetはtaskリストの親要素のid
       Turbo::StreamsChannel.broadcast_append_to :home, target: :tasks, partial: "tasks/task", locals: { task: @task }
-      flash.now.notice = "更新した"
+      flash.now.notice = "作成した"
     else
       @tasks = Task.all
       flash.now.alert = "失敗した"
       render :index, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash.now.notice = "削除した"
+    Turbo::StreamsChannel.broadcast_remove_to :home, target: @task
   end
 
   private
